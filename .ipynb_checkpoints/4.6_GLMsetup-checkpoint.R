5739cef0fccb2e79eaf338b70cbd7574dde9f3ef
Carolyn McNabb <58777305+CarolynMcNabb@users.noreply.github.com>
2021-06-24 12:16:42 +0100

    ADD: added Autism Quotient to analysis

--- .ipynb_checkpoints/4.6_GLMsetup-checkpoint.R
+++ .ipynb_checkpoints/4.6_GLMsetup-checkpoint.R
@@ -4,9 +4,9 @@ setwd("G:/My Drive/Pilot")
 library("Jmisc")
 library(dplyr)
 
-#read in demographic and gut diversity data
+#read in demographic, Autism quotient and gut diversity data
 pilot_raw <- read.csv("pilot_bacteria_mrs_lcms_GenusLevel.csv")
-diversity <- pilot_raw[c("age","hand","faith_pd","shannon","observed_otus")]
+diversity <- pilot_raw[c("age","hand","faith_pd","shannon","observed_otus", "aq")]
 
 #read in dwi motion data
 motion <- read.delim("G://My Drive/Diffusion/dti_motion.txt", header = F, sep = ",")
@@ -15,12 +15,14 @@ colnames(motion) <- c("sub", "motion_from_1st","motion_from_prev")
 #recode handedness from right and left to 0s and 1s
 diversity$hand <- recode(diversity$hand, Right = 0, Left = 1)
 
-#create dataframes for GLM, including diversity measures, age, handedness and motion, then demean these values for use in FSL's randomise
+#create dataframes for GLM, including diversity measures/Autism quotient, age, handedness and motion, then demean these values for use in FSL's randomise
 shannon_GLM <- demean(cbind(diversity$shannon, diversity$age, diversity$hand, motion$motion_from_1st, motion$motion_from_prev))
 otus_GLM <- demean(cbind(diversity$observed_otus, diversity$age, diversity$hand, motion$motion_from_1st, motion$motion_from_prev))
 faith_GLM <- demean(cbind(diversity$faith_pd, diversity$age, diversity$hand, motion$motion_from_1st, motion$motion_from_prev))
+aq_GLM <- demean(cbind(diversity$aq, diversity$age, diversity$hand, motion$motion_from_1st, motion$motion_from_prev))
 
 # write GLMs to csv format
 write.table( shannon_GLM, "GLMshannon.csv", sep=",", col.names=FALSE, row.names = FALSE)
 write.table( otus_GLM, "GLMotus.csv", sep=",", col.names=FALSE, row.names = FALSE)
 write.table( faith_GLM, "GLMfaith.csv", sep=",", col.names=FALSE, row.names = FALSE)
+write.table( aq_GLM, "GLMaq.csv", sep=",", col.names=FALSE, row.names = FALSE)
--- .ipynb_checkpoints/4.9_randomise-checkpoint.sh
+++ .ipynb_checkpoints/4.9_randomise-checkpoint.sh
@@ -18,9 +18,9 @@ echo "Running randomise for Shannon index of diversity"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/shannon -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_shannon.mat -t ${GLM_path}/TBSS_shannon.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 &
-echo "Shannon" `fslstats ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
-
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 shannon_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+echo "Shannon+" `fslstats ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
+echo "Shannon-" `fslstats ${stats_path}/shannon_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 
 ############# OTUs
 #run randomise to evaluate correlations between FA and OTUs index of diversity – the T2 option tells FSL that the images are T2 images 
@@ -28,9 +28,9 @@ echo "Running randomise for OTUs"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/otus -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_otus.mat -t ${GLM_path}/TBSS_otus.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/otus_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 &
-echo "OTUs" `fslstats ${stats_path}/otus_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
-
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/otus_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 otus_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+echo "OTUs+" `fslstats ${stats_path}/otus_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
+echo "OTUs-" `fslstats ${stats_path}/otus_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 
 ############# FAITH
 #run randomise to evaluate correlations between FA and faith index of diversity – the T2 option tells FSL that the images are T2 images 
@@ -38,5 +38,16 @@ echo "Running randomise for faith"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/faith -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_faith.mat -t ${GLM_path}/TBSS_faith.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/faith_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 &
-echo "Faith" `fslstats ${stats_path}/faith_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/faith_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 faith_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+echo "Faith+" `fslstats ${stats_path}/faith_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
+echo "Faith-" `fslstats ${stats_path}/faith_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
+
+############# AUTISM QUOTIENT
+#run randomise to evaluate correlations between FA and Autism Quotient – the T2 option tells FSL that the images are T2 images 
+echo "Running randomise for AQ"
+randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/aq -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_aq.mat -t ${GLM_path}/TBSS_aq.con -n 5000 --T2
+
+#visualise output and print the maximum value in a file called inferential_stats.txt
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/aq_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 aq_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+echo "AQ+" `fslstats ${stats_path}/aq_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
+echo "AQ-" `fslstats ${stats_path}/aq_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
