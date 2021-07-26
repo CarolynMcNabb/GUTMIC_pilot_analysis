288620fe7bdf59c5a955348bc6c436e138b3becb
Carolyn McNabb <58777305+CarolynMcNabb@users.noreply.github.com>
2021-07-26 14:57:33 +0100

    FIX: corrected paths

--- .ipynb_checkpoints/4.9_randomise-checkpoint.sh
+++ .ipynb_checkpoints/4.9_randomise-checkpoint.sh
@@ -18,7 +18,7 @@ echo "Running randomise for Shannon index of diversity"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/shannon -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_shannon.mat -t ${GLM_path}/TBSS_shannon.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 shannon_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 ${stats_path}/shannon_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
 echo "Shannon+" `fslstats ${stats_path}/shannon_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 echo "Shannon-" `fslstats ${stats_path}/shannon_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 
@@ -28,7 +28,7 @@ echo "Running randomise for OTUs"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/otus -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_otus.mat -t ${GLM_path}/TBSS_otus.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/otus_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 otus_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/otus_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 ${stats_path}/otus_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
 echo "OTUs+" `fslstats ${stats_path}/otus_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 echo "OTUs-" `fslstats ${stats_path}/otus_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 
@@ -38,7 +38,7 @@ echo "Running randomise for faith"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/faith -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_faith.mat -t ${GLM_path}/TBSS_faith.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/faith_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 faith_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/faith_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 ${stats_path}/faith_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
 echo "Faith+" `fslstats ${stats_path}/faith_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 echo "Faith-" `fslstats ${stats_path}/faith_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 
@@ -48,6 +48,6 @@ echo "Running randomise for AQ"
 randomise -i ${stats_path}/all_FA_skeletonised -o ${stats_path}/aq -m ${stats_path}/mean_FA_skeleton_mask -d ${GLM_path}/TBSS_aq.mat -t ${GLM_path}/TBSS_aq.con -n 5000 --T2
 
 #visualise output and print the maximum value in a file called inferential_stats.txt
-fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/aq_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 aq_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
+fsleyes $FSLDIR/data/standard/MNI152_T1_1mm ${stats_path}/mean_FA_skeleton --cmap green --displayRange 0.2 0.8 ${stats_path}/aq_tfce_corrp_tstat1.nii.gz --cmap red-yellow --displayRange 0.95 1 ${stats_path}/aq_tfce_corrp_tstat2.nii.gz --cmap blue-lightblue --displayRange 0.95 1 &
 echo "AQ+" `fslstats ${stats_path}/aq_tfce_corrp_tstat1.nii.gz -R` >> ${stats_path}/inferential_stats.txt
 echo "AQ-" `fslstats ${stats_path}/aq_tfce_corrp_tstat2.nii.gz -R` >> ${stats_path}/inferential_stats.txt
